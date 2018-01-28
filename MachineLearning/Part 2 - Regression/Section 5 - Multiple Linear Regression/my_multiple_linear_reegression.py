e5afbc749831a103286153b4405e3b854d9c3241
jonathanformigari <jonathan.formigari@gmail.com>
2018-01-28 16:11:10 -0200

    Regression classes

---
+++ MachineLearning/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_reegression.py
@@ -0,0 +1,15 @@
+#Simple linear regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('C:\\Users\\Jonathan\\Documents\\MyProject\\MyPortfolio\\MachineLearning\\Part 2 - Regression\\Section 4 - Simple Linear Regression\\Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
