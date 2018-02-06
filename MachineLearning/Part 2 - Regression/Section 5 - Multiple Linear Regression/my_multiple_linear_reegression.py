44504e821fee1667e4e56c68640120da51ee8d7f
jonathanformigari <jonathan.formigari@gmail.com>
2018-02-05 23:17:31 -0200

    Backward Elimination formula

--- MachineLearning/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_reegression.py
+++ MachineLearning/Part 2 - Regression/Section 5 - Multiple Linear Regression/my_multiple_linear_reegression.py
@@ -22,10 +22,6 @@ X = onehotencoder.fit_transform(X).toarray()
 #Avoiding the Dummy Variable Trap
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
 '''
 Build a Model:
 
@@ -62,6 +58,109 @@ Build a Model:
     ii.     Construct all possible regression Models 2^N-1 total combinations
     iii.    Select the one with the best criterion
     iv.     FIN:Your Model is Ready
+'''
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting Multiple Linear Reegression the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train) 
+
+#Predicting the best set results
+y_pred = regressor.predict(X_test)
+
+#Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+
+#Adding the b_0 factor using a (1) column  (f = b_0 + b_1*x_1...)
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+#Complete Model
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+# First iteration
+X_opt = X[:, [0, 1, 3, 4, 5]]
+
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+# Second Iteration
+X_opt = X[:, [0, 3, 4, 5]]
+
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#Third Iteration
+X_opt = X[:, [0, 3, 5]]
+
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#Fourth Iteration
+X_opt = X[:, [0, 3]]
+
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+print(regressor_OLS.summary())
+
+'''
+Automatic model:
+
+import statsmodel.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+
+    regressor_OLS.summary()
+    return x
+
+
+SL = 0.05
+X_opt = X[:, ...]
+X_Modeled = bacwardElimination(X_opt, SL)
+
+---------------------------------------------------------------------------------------------
+
+Model with P and R-Squared:
+
+import statsmodels.formula.api as sm
+def backwardElimination(x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+
 '''
 
 print(X)
