dc1de2f334fff55661ce78a5050e84c108177386
jonathanformigari <jonathan.formigari@gmail.com>
2018-02-16 08:05:07 -0200

    decision_tree_regression

--- MachineLearning/Part 2 - Regression/Section 8 - Decision Tree Regression/regression_template.py
+++ MachineLearning/Part 2 - Regression/Section 8 - Decision Tree Regression/regression_template.py
@@ -1,4 +1,4 @@
-# SVR Regression
+# Regression Template
 
 # Importing the libraries
 import numpy as np
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('C:\\Users\\Jonathan\\Documents\\MyProject\\MyPortfolio\\MachineLearning\\Part 2 - Regression\\Section 7 - Support Vector Regression (SVR)\\Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
@@ -15,19 +15,18 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler # the SVR regression class does not scale automatic
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = np.ravel(sc_y.fit_transform(y.reshape(-1, 1)))  
+y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Regression Model to the dataset
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf') # rbf = gaussian
-regressor.fit(X, y)
+# Fitting the Regression Model to the dataset
+# Create your regressor here
 
-# Predicting a new result with SVR Regression
-y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+# Predicting a new result
+y_pred = regressor.predict(6.5)
 
 # Visualising the Regression results
 plt.scatter(X, y, color = 'red')
@@ -37,8 +36,7 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-'''
-# Visualising the Regression results (for higher resolution)
+# Visualising the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
@@ -47,4 +45,3 @@ plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-'''
